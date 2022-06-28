ff394e61092b435c84dbc545c1643a912369a45e
rodion <rodion.solovev.7@github.com>
2022-06-29 01:53:50 +0500

    some changes

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+import os
 import time
 import typing
 from collections import deque
 
-from PIL import ImageGrab
-import pyautogui
 import cv2
 import numpy as np
+import pyautogui
+import pyrect
+from PIL import ImageGrab, Image, ImageFont, ImageDraw
 from pywinctl import Window
 
 
@@ -55,51 +57,77 @@ class FpsMeter:
 
 
 def get_screen_image(bbox: BoundingBox = None) -> np.ndarray:
-    # noinspection PyTypeChecker
-    image = np.array(ImageGrab.grab(bbox))
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    image = cv2.cvtColor(np.array(ImageGrab.grab(bbox)), cv2.COLOR_RGB2BGR)
     return image
 
 
-# noinspection PyMissingConstructor
 class WindowCapture(cv2.VideoCapture):
-    def __init__(self, win_or_name: typing.Union[str, Window]):
-        if isinstance(win_or_name, str):
-            win_or_name = pyautogui.getWindowsWithTitle(win_or_name)[0]
-        self.window = win_or_name
+    # noinspection PyMissingConstructor
+    def __init__(self, window_or_name: typing.Union[str, Window]):
+        if isinstance(window_or_name, str):
+            window_or_name = pyautogui.getWindowsWithTitle(window_or_name)[0]
+        self._window = window_or_name
 
     def read(self, _=None) -> tuple[bool, np.ndarray]:
-        x, y, w, h = self.window.box
+        x, y, w, h = self._window.box
         return True, get_screen_image(BoundingBox(x, y, x + w, y + h))
 
+    def window_box(self) -> pyrect.Box:
+        return self._window.box
+
     def __repr__(self) -> str:
-        return repr(self.window.box)
+        return (
+            f'<object {self.__class__.__name__}('
+            f'title={self._window.title!r} '
+            f'box={self._window.box!r}'
+            f')>'
+        )
+
+
+def draw_fps(image: np.ndarray, meter: FpsMeter) -> None:
+    meter.tick()
+    fps_text = f'FPS={meter.fps:.2f}\nDELAY={meter.delay:.2f}'
+
+    font_path = os.path.join(cv2.__path__[0], 'qt', 'fonts', 'DejaVuSans.ttf')
+    font = ImageFont.truetype(font_path, size=80)
+
+    pil_img = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    draw = ImageDraw.Draw(pil_img)
+    draw.text((0, 0), fps_text, fill='red', anchor='la', font=font)
+
+    image[:, :, :] = cv2.cvtColor(np.asarray(pil_img), cv2.COLOR_RGB2BGR)
 
 
 def process_capture() -> None:
-    cap = WindowCapture(pyautogui.getActiveWindow())
     fps_meter = FpsMeter()
+    cap = WindowCapture(pyautogui.getActiveWindow())
+
+    sizer = 0.4
+    *_, width, height = cap.window_box()
+    window_name = 'image'
+    cv2.namedWindow(window_name, cv2.WINDOW_NORMAL | cv2.WINDOW_GUI_NORMAL)
+    cv2.resizeWindow(window_name, int(width * sizer), int(height * sizer))
 
     while True:
         has_read, image = cap.read()
 
-        fps_meter.tick()
-        fps_text = f'FPS={fps_meter.fps:02.2f} DELAY={fps_meter.delay:.2f}'
-        draw_text(image, (50, 50), fps_text)
-
-        cv2.imshow('image', image)
+        draw_fps(image, fps_meter)
+        cv2.imshow(window_name, image)
 
         key = cv2.waitKey(1)
         if key in (27, ord('q'), ord('Q')):
-            cv2.destroyAllWindows()
-            raise KeyboardInterrupt()
+            return
+        if key in (ord(' '), ord('p'), ord('P')):
+            cv2.waitKey()
 
 
-def main():
+def main() -> None:
     try:
         process_capture()
     except KeyboardInterrupt:
         pass
+    finally:
+        cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
